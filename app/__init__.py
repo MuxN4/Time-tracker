3d868e477216912e29db3aead90206d5e1dd5e90
Steve <stev3npy@gmail.com>
2024-07-18 10:03:19 +0300

    Configured app from config object and imported models

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+
+from app import models
 
 from app import routes
